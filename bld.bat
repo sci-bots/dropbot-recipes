486a37e9ccecab818697b55e092e0d056519593f
Christian Fobel <christian@fobel.net>
2018-02-12 03:34:40 -0500

    fix(unzip): use 7za on Windows, unzip otherwise

--- bld.bat
+++ bld.bat
@@ -1,7 +1,7 @@
 REM Download Windows (32-bit) binary.
 "%PYTHON%" -m wget https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-win32.zip
 if errorlevel 1 exit 1
-REM Extract $_OUTDIR\bin\libclang.dll from LZMA archive.
+REM Extract from ZIP archive.
 "%PREFIX%"\Library\bin\7za x protoc-3.1.0-win32.zip -y
 if errorlevel 1 exit 1
 REM Copy protoc.exe to ${PREFIX}\Library\bin
