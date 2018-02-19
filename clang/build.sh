0309252abe5e0f889dc966206a2efd68c9faf10e
Christian Fobel <christian@fobel.net>
2018-02-19 16:55:30 -0500

    refactor(dll): move shared lib to clang package

---
+++ clang/build.sh
@@ -0,0 +1,17 @@
+export SOURCE_ROOT=$(pwd)
+
+# Clone source for clang release 3.9.0.
+git clone -b release_39 --depth 1 https://github.com/llvm-mirror/clang clang-git
+rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
+
+mkdir build
+cd build
+# Generate make files using cmake.
+cmake -G "Unix Makefiles" ../clang-git
+rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
+# Build clang.
+make
+rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
+
+cp -a ${SOURCE_ROOT}/build/lib/libclang.so* ${PREFIX}/lib
+rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
