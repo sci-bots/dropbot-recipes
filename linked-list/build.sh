39b64484ce0ff5d6d95c185d8fada84a0eeb46b8
Christian Fobel <christian@fobel.net>
2018-02-20 14:13:53 -0500

    feat(linked-list): update version to 1.2.3

--- linked-list/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p "${PREFIX}"/include/Arduino
-cp -ra "${SRC_DIR}" "${PREFIX}"/include/Arduino/LinkedList
-rm "${PREFIX}"/include/Arduino/LinkedList/build.sh
-rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
