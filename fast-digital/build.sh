3b9ad354c82d22bd0d914d6c5e5e35b95c1e5f0f
Christian Fobel <christian@fobel.net>
2018-02-27 09:48:20 -0500

    fix(fast-digital): remove build.script selectors
    To support `conda-build-all`, remove selectors (e.g., `# [win]`,
    `# [not win]`) from `build.script` string.

--- fast-digital/build.sh
+++
@@ -1,5 +0,0 @@
-export LIB_NAME=FastDigital
-mkdir -p "${PREFIX}"/include/Arduino
-cp -ra "${SRC_DIR}" "${PREFIX}"/include/Arduino/${LIB_NAME}
-rm "${PREFIX}"/include/Arduino/${LIB_NAME}/build.sh
-rc=$?; if [[ $rc != 0  ]]; then exit $rc; fi
