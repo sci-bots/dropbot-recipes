ac07ee714de3aa42ba967a7a5264ffed802e93b4
Christian Fobel <christian@fobel.net>
2018-02-28 15:15:45 -0500

    fix(clang): download win exe based on arch

--- clang/bld.bat
+++ clang/bld.bat
@@ -5,11 +5,11 @@ set URL=http://llvm.org/releases/%PKG_VERSION%/LLVM-%PKG_VERSION%-win%ARCH%.exe
 
 echo %URL%
 
-REM Download Windows (32-bit) binary.
-curl -L -o LLVM-%PKG_VERSION%-win32.exe %URL%
+REM Download Windows binary for recipe architecture (32 or 64).
+curl -L -o LLVM-%PKG_VERSION%-win%ARCH%.exe %URL%
 if errorlevel 1 exit 1
 REM Extract $_OUTDIR\bin\libclang.dll from LZMA archive.
-%PREFIX%\Library\usr\lib\p7zip\7z.exe e LLVM-%PKG_VERSION%-win32.exe libclang.dll -y -r
+%PREFIX%\Library\usr\lib\p7zip\7z.exe e LLVM-%PKG_VERSION%-win%ARCH%.exe libclang.dll -y -r
 if errorlevel 1 exit 1
 REM Copy libclang.dll to ${PREFIX}\Library\bin\libclang.dll.
 copy libclang.dll "%PREFIX%"\Library\bin\libclang.dll
