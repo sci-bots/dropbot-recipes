4c846d59501e59a6e6e9497231722a59749d08f7
Christian Fobel <christian@fobel.net>
2018-03-05 11:31:32 -0500

    fix(nanopb): use libprotobuf (not protoc)
    The `protoc` binary executable is included in the `libprotobuf` package,
    so the `protoc` package is no longer required.
    
    Fixes sci-bots/nanopb-recipe#1.

--- protoc/bld.bat
+++
@@ -1,5 +0,0 @@
-REM Copy protoc.exe to ${PREFIX}\Library\bin
-copy bin\protoc.exe "%PREFIX%"\Library\bin\protoc.exe
-if errorlevel 1 exit 1
-xcopy /S /Y /I /Q include\google "%PREFIX%"\Library\include\google
-if errorlevel 1 exit 1
